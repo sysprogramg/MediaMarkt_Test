7d8fa9a582ef3b280d15b0286aa7490d42151466
Rafael Esviza <sysprogramg@gmail.com>
2021-12-25 20:26:57 +0100

    Add SQL DataBase Project

---
+++ MediaMarkt_Test.DataBase/StoredProcedures/product_GetByName.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE product_GetByName
+		@strName		NVARCHAR(30) = NULL
+	,	@strDescription	NVARCHAR(75) = NULL
+AS
+	SELECT
+			[id]
+		,	[name]
+		,	[description]
+		,	[price]
+		,	[family]
+	FROM
+		product
+	WHERE
+			(@strName IS NULL OR [name] LIKE '%' + @strName + '%')
+		OR
+			(@strDescription IS NULL OR [description] LIKE '%' + @strDescription + '%');
